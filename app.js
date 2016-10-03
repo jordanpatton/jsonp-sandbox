0b7010757563e0ade04d6a1ffb0b82e2019e1d52
Jordan Patton <jordanpatton@gmail.com>
2016-10-02 18:16:05 -0700

    content

---
+++ app.js
@@ -0,0 +1,55 @@
+/* dependencies */
+var express = require('express');
+var path    = require('path');
+
+/* environment variables */
+var CONFIG_PORT = parseInt((process.env.CONFIG_PORT || 3000), 10);
+if (isNaN(CONFIG_PORT)) {
+  console.log('invalid port:', CONFIG_PORT);
+  process.exit(1);
+}
+
+/* express configuration */
+var app = express();
+
+/* express middleware */
+app.use(function (req, res, next) {
+  /* miscellaneous response headers */
+  res.setHeader('Cache-Control', 'no-cache');
+  next();
+});
+
+/* express routing */
+app.get('/js/generated-script.js', function (req, res, next) {
+  if (typeof req.query.requestId !== 'undefined') {
+    var requestId = req.query.requestId;
+    delete req.query.requestId;
+    res.writeHead(200, {'Content-Type': 'text/javascript'});
+    res.write(
+        'ScriptInjector = ScriptInjector || {};' +
+        'ScriptInjector.ingest = ScriptInjector.ingest || {};' +
+        'ScriptInjector.ingest[\''+requestId+'\'] = '+JSON.stringify(req.query)+';'
+    );
+    res.end();
+  } else {
+    res.sendStatus(400);
+  }
+});
+
+/* express static file server */
+app.use(
+  express.static(
+    path.join(__dirname, 'public')
+  )
+);
+
+/* express invocation */
+app.listen(CONFIG_PORT, function () {
+  console.log([
+    '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~',
+    'script-injector-sandbox running',
+    ' => http://localhost:' + CONFIG_PORT,
+    ' => [ ctrl + c ] to quit',
+    '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+  ].join('\n'));
+});
---
+++ public/js/script-injector.js
@@ -0,0 +1,119 @@
+/**
+ * Script Injector
+ */
+window.ScriptInjector = (function (window, document) {
+
+  /* ========== */
+  /* Properties */
+  /* ========== */
+
+  /**
+   * Stores all of the inbound data from the server.
+   */
+  var ingest = {};
+
+
+  /* =================== */
+  /* Convenience Methods */
+  /* =================== */
+
+  /**
+   * Randomly generates a GUID v4.
+   * @returns {string} Randomly-generated GUID v4.
+   */
+  function guidv4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r=Math.random()*16|0,v=c=='x'?r:r&0x3|0x8;
+      return v.toString(16);
+    });
+  }
+
+  /**
+   * Injects a script into the DOM with given options.
+   * @param {Object} options Required.
+   * @param {string} options.src Required.
+   * @param {boolean} options.async Optional.
+   * @param {function} options.callback Optional.
+   * @param {boolean} options.defer Optional.
+   */
+  function injectScript(options) {
+    if (typeof options === 'undefined' || typeof options.src === 'undefined') {
+      throw new TypeError('injectScript missing one of: options, options.src');
+    }
+
+    var element = document.createElement('script');
+    element.src = options.src;
+    element.async = options.async || false;
+    element.defer = options.defer || false;
+
+    if (Object.prototype.toString.call(options.callback) === '[object Function]') {
+      element.onload = options.callback;
+    }
+
+    document.head.appendChild(element);
+  }
+
+  /**
+   * Recursively serializes data as a query-parameter string.
+   * @param {Object} obj Data to serialize (required).
+   * @param {string} pfx Key prefix for data chunk (optional).
+   * @returns {string} Query-parameter string.
+   */
+  function serialize(obj, pfx) {
+    var results = [];
+    for(var prop in obj) {
+      if (obj.hasOwnProperty(prop)) {
+        var key = pfx ? (pfx + '[' + prop + ']') : prop;
+        var val = obj[prop]
+        results.push(
+          (typeof val === 'object') ?
+          serialize(val, key) :
+          (encodeURIComponent(key) + '=' + encodeURIComponent(val))
+        );
+      }
+    }
+    return results.join('&');
+  }
+
+
+  /* ================ */
+  /* Workflow Methods */
+  /* ================ */
+
+  /**
+   * Performs a script-injection request.
+   * @param {Object} data JSON-compatible data payload (optional).
+   * @param {function} successCallback Required.
+   * @param {function} failureCallback Required.
+   */
+  function request(data, successCallback, failureCallback) {
+    if (typeof successCallback === 'undefined' || typeof failureCallback === 'undefined') {
+      throw new TypeError('request missing one of: successCallback, failureCallback');
+    }
+
+    var requestId = guidv4();
+    var callback = function() {
+      if (ingest && ingest[requestId]) {
+        return successCallback(ingest[requestId]);
+      } else {
+        return failureCallback();
+      }
+    };
+
+    return injectScript({
+      src: '/js/generated-script.js?requestId='+requestId+'&'+serialize(data),
+      callback: callback
+    });
+  }
+
+
+  /* ===================================== */
+  /* Publicly-Exposed Properties & Methods */
+  /* ===================================== */
+
+  return {
+    'ingest': ingest,
+    'request': request
+  };
+
+})(window, window.document);
